c899b4644d1cd3f4c914d4360ec82e10822c64ad
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-18 18:59:51 -0500

    feat: Updated FastAPI to use OAuth2 with JWT, also updated the name of the files that contain the routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI, Depends
-from app.routes import administrator, user, seller
+from app.routes import route_administrator
 from app.controllers.users_controller import register_user, login
 from app.models.user import UserCreate, User
 from app.models.token import Token
 from app.db.db_conexion import get_db
+from app.routes import route_seller, route_user
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
@@ -20,9 +33,9 @@ async def post_login(correo: str, contrasenna: str, db = Depends(get_db)):
     return login(correo, contrasenna, db)
 
 # routes
-app.include_router(administrator.router, prefix="/administrator", tags=["Administrator"])
-app.include_router(user.router, prefix="/user", tags=["User"])
-app.include_router(seller.router, prefix="/seller", tags=["Seller"])
+app.include_router(route_administrator.router, prefix="/administrator", tags=["Administrator"])
+app.include_router(route_user.router, prefix="/user", tags=["User"])
+app.include_router(route_seller.router, prefix="/seller", tags=["Seller"])
 
 # Start development server using Uvicorn
 if __name__ == "__main__":
