46d295e5229004cc62b1e7eca7db9cdae348c3c8
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-17 21:16:46 -0500

    fix: updating the endpoints "/", "registered user", "login". The corresponding imports were also updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from app.routes import administrator, user, seller
+from app.controllers.users_controller import register_user, login
+from app.models.user import UserCreate, User
+from app.models.token import Token
+from app.db.db_conexion import get_db
 
 app = FastAPI()
 
-@app.post("/register_user")
-async def post_register_user():
-    pass
+@app.get("/")
+async def read_root():
+    return {"Welcome to the API Marketplace Artesanias"}
 
-@app.post("/login")
-async def post_login():
-    pass
+@app.post("/register_user", response_model=User)
+async def post_register_user(user_in: UserCreate, db = Depends(get_db)):
+    return register_user(user_in, db)
 
+@app.post("/login", response_model=Token)
+async def post_login(correo: str, contrasenna: str, db = Depends(get_db)):
+    return login(correo, contrasenna, db)
 
 # routes
 app.include_router(administrator.router, prefix="/administrator", tags=["Administrator"])
@@ -18,6 +25,6 @@ app.include_router(user.router, prefix="/user", tags=["User"])
 app.include_router(seller.router, prefix="/seller", tags=["Seller"])
 
 # Start development server using Uvicorn
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='localhost', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000, reload=True)
