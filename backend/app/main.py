fe757e2e8f508f395922cd1fdc916bdca7058a96
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-20 20:36:08 -0500

    fix: refactoring the emponits to have more order

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from app.routes import route_user
+from app.routes import route_user, route_seller, route_store, route_administrator
+from app.models.user import UserCreate
+from app.controllers import user_controller
+from app.db.db_conexion import get_db
 
 app = FastAPI()
 
@@ -12,20 +15,31 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-@app.get("/")
-async def read_root():
-    return {"Welcome to the API Marketplace Artesanias"}
-
+# empoint para registrar un usuario.
 @app.post("/register_user")
-async def post_register_user():
-    pass
+async def post_register_user(user: UserCreate,  db: any = Depends(get_db)):
+    return user_controller.create_user(user, db)
 
+# empoint para iniciar sesion.
 @app.post("/login")
 async def post_login():
     pass
 
-# routes
+# empoint para ver tiendas.
+@app.get("/see_stores")
+async def get_see_stores():
+    pass
+
+# empoint para aver productos.
+@app.get("/see_products")
+async def get_see_products():
+    pass
+
+# rutas protegidas que contienen empoints que seran usason solo si el usuario inicio sesion
 app.include_router(route_user.router, prefix="/user", tags=["User"])
+app.include_router(route_seller.router, prefix="/seller", tags=["Seller (User)"])
+app.include_router(route_store.router, prefix="/store", tags=["Store"])
+app.include_router(route_administrator.router, prefix="/administrator", tags=["Administrator (User)"])
 
 if __name__ == "__main__":
     import uvicorn
