fdce81d881839da7aa3bcfb01f48dc9c5f6870ae
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-20 20:35:17 -0500

    feat: encrypting passwords

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -2,8 +2,5 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
