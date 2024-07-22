6820cbc7c380ed1a9394a8006f04da96c419ffb1
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-21 20:05:23 -0500

    fix: updating comments

--- backend/app/routes/route_seller.py
+++ backend/app/routes/route_seller.py
@@ -9,8 +9,8 @@ router = APIRouter()
 # Ruta para crear una tienda.
 @router.post("/create_store", response_model=dict)
 async def post_create_store(store: StoreCreate, current_user: dict = Depends(get_current_user), db: any = Depends(get_db)):
-    # Verificar si el usuario tiene el rol adecuado para crear una tienda
-    if current_user["rol_id"] != 3:  # Suponiendo que el rol "seller" tiene el rol_id 3
+    # Verificar si el usuario es una vendedor (seller)
+    if current_user["rol_id"] != 3:  # 3 es el rol_id de para vendedores
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to create a store")
     return create_store(store, current_user, db)
 
