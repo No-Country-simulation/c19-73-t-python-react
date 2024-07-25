c28915b66653f31a915c2bec75d6b81318397ed4
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-25 18:27:54 -0500

    feat: implementation of the "update_product" function in the controller to update product data

--- backend/app/controllers/product_controller.py
+++ backend/app/controllers/product_controller.py
@@ -1,5 +1,5 @@
 from fastapi import HTTPException, status
-from app.models.product import ProductoCreate
+from app.models.product import ProductoCreate, ProductoUpdate
 from app.utils.security import validate_seller_role, validate_store_ownership
 
 def create_product(product: ProductoCreate, current_user: dict, db: any):
@@ -15,6 +15,37 @@ def create_product(product: ProductoCreate, current_user: dict, db: any):
         db.commit()
         return {"message": "Product created successfully"}
     except Error as e:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    finally:
+        cursor.close()
+
+def update_product(product: ProductoUpdate, current_user: dict, db: any):
+    validate_seller_role(current_user)
+    
+    cursor = db.cursor()
+    try:
+        # Verificar si el producto pertenece a una tienda del usuario
+        cursor.execute("""
+            SELECT id_tienda FROM productos WHERE id_producto = %s
+        """, (product.id_producto,))
+        result = cursor.fetchone()
+        if not result:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No tienes productos disponibles para actualizar")
+        
+        id_tienda = result[0]
+        validate_store_ownership(id_tienda, current_user['id_usuario'], db)
+
+        print(f"Updating product: {product}")
+        cursor.execute("""
+            UPDATE productos
+            SET nombre_producto = %s, descripcion_producto = %s, ruta_foto_principal = %s, precio = %s, stock = %s
+            WHERE id_producto = %s
+        """, (product.nombre_producto, product.descripcion_producto, product.ruta_foto_principal, product.precio, product.stock, product.id_producto))
+        db.commit()
+        return {"message": "Product updated successfully"}
+    except Error as e:
+        print(f"Database error: {e}")
         db.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
     finally:
