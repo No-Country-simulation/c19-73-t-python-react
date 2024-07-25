737994ec62eb4b529b8b48fad51dd7d043f565ab
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-25 18:29:19 -0500

    feat: implementation of the "update_product_store" endpoint to receive updated product information

--- backend/app/routes/route_store.py
+++ backend/app/routes/route_store.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends
 from app.auth.auth import get_current_user
 from app.db.db_conexion import get_db
 from app.models.store import StoreUpdate
-from app.models.product import ProductoCreate
+from app.models.product import ProductoCreate, ProductoUpdate
 from app.controllers.store_controller import update_store
-from app.controllers.product_controller import create_product
+from app.controllers.product_controller import create_product, update_product
 
 router = APIRouter()
 
@@ -19,9 +19,9 @@ async def create_product_store(product: ProductoCreate, current_user: dict = Dep
     return create_product(product, current_user, db)
 
 # empoint para actualizar productos de la tienda.
-@router.put("/update_product_store")
-async def put_update_product_store():
-    pass
+@router.put("/update_product_store", response_model=dict)
+async def put_update_product_store(product_update: ProductoUpdate, current_user: dict = Depends(get_current_user), db: any = Depends(get_db)):
+    return update_product(product_update, current_user, db)
 
 # empoint para eliminar productos de la tienda.
 @router.delete("/delete_product_store")
