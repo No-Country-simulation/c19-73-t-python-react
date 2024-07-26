71146701ec94fc64e1a893439c1b8a2ad5e9b999
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-25 20:54:29 -0500

    feat: implementing "SeeProduct" model to see products from a specific store

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
 class ProductoCreate(BaseModel):
     id_tienda: int
@@ -17,6 +18,16 @@ class ProductoUpdate(BaseModel):
     nombre_producto: str
     descripcion_producto: str
     ruta_foto_principal: str
+    precio: float
+    stock: int
+
+class SeeProduct(BaseModel):
+    id_producto: int
+    id_tienda: int
+    id_categoria_producto: int
+    nombre_producto: str
+    descripcion_producto: Optional[str]
+    ruta_foto_principal: Optional[str]
     precio: float
     stock: int
 
