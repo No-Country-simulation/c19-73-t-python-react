b9fc960236376a53dbb8068c804481bc705a4032
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-28 12:13:52 -0500

    feat: implementing "notify_seller" function to enter a notification to the database

--- backend/app/controllers/administrator_controller.py
+++ backend/app/controllers/administrator_controller.py
@@ -1,5 +1,5 @@
-# administrator_controller.py
 from fastapi import HTTPException, status
+from app.models.notification import NotificationSellerCreate
 from mysql.connector import Error
 
 def delete_store_admin(store_id: int, db):
@@ -11,6 +11,31 @@ def delete_store_admin(store_id: int, db):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Store not found")
         return {"detail": "Store deleted successfully"}
     except Error as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    finally:
+        cursor.close()
+
+def notify_seller(notification: NotificationSellerCreate, db, user_id: int):
+    cursor = db.cursor(dictionary=True)
+    
+    try:
+        # Verificamos que el usuario con el id proporcionado tenga el rol 'seller'
+        cursor.execute("SELECT id_usuario FROM usuarios WHERE id_usuario = %s AND rol_id = 3", (user_id,))
+        seller = cursor.fetchone()
+        
+        if not seller:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Seller not found or not authorized")
+        
+        # Insertar una notificación para el vendedor específico
+        cursor.execute(
+            "INSERT INTO notificaciones (id_usuario, asunto, mensaje) VALUES (%s, %s, %s)",
+            (seller['id_usuario'], notification.asunto, notification.mensaje)
+        )
+        
+        db.commit()
+        return {"message": "Notification sent successfully"}
+    except Error as e:
+        db.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
     finally:
         cursor.close()
