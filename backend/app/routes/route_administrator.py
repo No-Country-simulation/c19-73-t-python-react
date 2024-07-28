83f969bc18dc3fafe2bf461d5c6195a28ef039b2
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-27 22:58:42 -0500

    feat: implementation of the "delete_product" empoint so that the administrator can delete a product from any store

--- backend/app/routes/route_administrator.py
+++ backend/app/routes/route_administrator.py
@@ -6,24 +6,32 @@ from app.models.store import StoreStateUpdate
 from app.models.user import User
 from app.controllers.category_controller import create_category
 from app.controllers.store_controller import update_store_state
+from app.controllers.product_controller import delete_product
 
 router = APIRouter()
 
+# Empoint para crear categorias para los productos.
 @router.post("/create_categories_products")
 async def create_categories_products(category: CategoriaProductoCreate, current_user: dict = Depends(get_current_user), db: any = Depends(get_db)):
     return create_category(category, current_user, db)
 
-# empoint para actualizar estado de la tienda tienda.
+# Empoint para actualizar estado de la tienda tienda.
 @router.put("/update_state_store/{id_tienda}")
 async def update_state_store(id_tienda: int, state_update: StoreStateUpdate, db: any = Depends(get_db), current_user: User = Depends(get_current_user)):
     if current_user["rol_id"] != 1: # Solo los administradores puedes usar este empoint
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to update store state")
     return update_store_state(id_tienda, state_update, db)
 
-# empoint para eliminar cualquier producto.
-@router.delete("/delete_product")
-async def delete_delete_product():
-    pass
+# Endpoint para eliminar un producto
+@router.delete("/delete_product/{product_id}")
+async def delete_product_endpoint(product_id: int, db: any = Depends(get_db), current_user: dict = Depends(get_current_user)):
+    print(f"Endpoint reached with product_id: {product_id}")  # Mensaje de depuración
+    print(f"Current user: {current_user}")  # Mensaje de depuración
+    if current_user['rol_id'] != 1:  # Verificar si el usuario es admin
+        print("User is not authorized to delete products")  # Mensaje de depuración
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete products")
+    print("User authorized to delete products")  # Mensaje de depuración
+    return delete_product(product_id, db)
 
 # empoint para eliminar cualquier tienda.
 @router.delete("/delete_store")
