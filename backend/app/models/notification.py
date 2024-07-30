4297fe8d6df9ad8dd78fe3a9d53f8f5529593ce6
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-30 07:52:57 -0500

    feat: implementando empoint "see_notifications" para que el usuario con rol "seller" pueda ver las notificaciones que le envie el administrador

--- backend/app/models/notification.py
+++ backend/app/models/notification.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
 
+class Notification(BaseModel):
+    id_notificacion: int
+    id_usuario: int
+    asunto: str
+    mensaje: str
+
 class NotificationSellerCreate(BaseModel):
     asunto: str
     mensaje: str
+
--- backend/app/routes/route_seller.py
+++ backend/app/routes/route_seller.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends , HTTPException, status
 from app.models.store import StoreCreate
+from app.models.user import User
 from app.auth.auth import get_current_user
 from app.db.db_conexion import get_db
 from app.controllers.store_controller import create_store
+from app.controllers.user_controller import get_notifications
 
 router = APIRouter()
 
@@ -15,6 +17,13 @@ async def post_create_store(store: StoreCreate, current_user: dict = Depends(get
     return create_store(store, current_user, db)
 
 # empoint para ver notificaciones del usurio vendedor.
-@router.get("/see_notifications")
-async def get_see_notifications_seller():
-    pass
+@router.get("/see_notifications/{user_id}")
+def see_notifications(user_id: int, db = Depends(get_db), current_user: dict = Depends(get_current_user)):
+    # Validar si el usuario actual es el mismo que el del token
+    if current_user["id_usuario"] != user_id:
+        raise HTTPException(status_code=403, detail="Not authorized to view these notifications")
+    # Validar si el usuario tiene el rol de "seller"
+    if current_user["rol_id"] != 3:  # Suponiendo que el rol de 'seller' tiene el ID 3
+        raise HTTPException(status_code=403, detail="Not authorized to view these notifications")
+    # Llamar a la función para obtener las notificaciones
+    return get_notifications(user_id, db)
