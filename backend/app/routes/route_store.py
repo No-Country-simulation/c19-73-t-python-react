a4a24f34cbb9d45de580378a41fa9c7db46c3be9
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-07-30 13:24:28 -0500

    fix: updating comment and model name

--- backend/app/routes/route_store.py
+++ backend/app/routes/route_store.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from app.auth.auth import get_current_user
 from app.db.db_conexion import get_db
 from app.models.store import StoreUpdate
-from app.models.product import ProductoCreate, ProductoUpdate, SeeProduct
+from app.models.product import ProductCreate, ProductUpdate
 from app.controllers.store_controller import update_store
 from app.controllers.product_controller import create_product, update_product, delete_product_store_seller
 
@@ -13,14 +13,14 @@ router = APIRouter()
 async def put_update_profile_store(store_id: int, store_update: StoreUpdate, current_user: dict = Depends(get_current_user), db: any = Depends(get_db)):
     return update_store(store_update, store_id, current_user['id_usuario'], db)
 
-
+# empoint para crear un producto de la tienda.
 @router.post("/create_product_store")
-async def create_product_store(product: ProductoCreate, current_user: dict = Depends(get_current_user), db: any = Depends(get_db)):
+async def create_product_store(product: ProductCreate, current_user: dict = Depends(get_current_user), db: any = Depends(get_db)):
     return create_product(product, current_user, db)
 
 # empoint para actualizar productos de la tienda.
 @router.put("/update_product_store", response_model=dict)
-async def put_update_product_store(product_update: ProductoUpdate, current_user: dict = Depends(get_current_user), db: any = Depends(get_db)):
+async def put_update_product_store(product_update: ProductUpdate, current_user: dict = Depends(get_current_user), db: any = Depends(get_db)):
     return update_product(product_update, current_user, db)
 
 # empoint para eliminar productos de la tienda.
