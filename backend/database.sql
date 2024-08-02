415047b8c655c297957a94ce45830f5e8f43b7b5
pablobautistagoyoneche <pablo.bautista.mkt@gmail.com>
2024-08-02 13:02:40 -0500

    feat: implementing docker for backend and database

---
+++ backend/database.sql
@@ -0,0 +1,103 @@
+DROP TABLE IF EXISTS rol;
+CREATE TABLE rol (
+        rol_id INT AUTO_INCREMENT PRIMARY KEY,
+        nombre VARCHAR(100) NOT NULL
+);
+
+DROP TABLE IF EXISTS estados_tienda;
+CREATE TABLE estados_tienda (
+        id_estado_tienda INT AUTO_INCREMENT PRIMARY KEY,
+        nombre_estado_tienda VARCHAR(256) NOT NULL
+);
+
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios (
+        id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+        nombre VARCHAR(100) NOT NULL,
+        telefono VARCHAR(100) NOT NULL,
+        direccion VARCHAR(256) NOT NULL,
+        correo VARCHAR(100) NOT NULL,
+        contrasenna VARCHAR(100) NOT NULL,
+        rol_id INT NOT NULL,
+        FOREIGN KEY (rol_id) REFERENCES rol(rol_id)
+);
+
+DROP TABLE IF EXISTS notificaciones;
+CREATE TABLE notificaciones (
+        id_notificacion INT AUTO_INCREMENT PRIMARY KEY,
+        id_usuario INT NOT NULL,
+        asunto VARCHAR(100) NOT NULL,
+        mensaje TEXT NOT NULL,
+        FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
+);
+
+DROP TABLE IF EXISTS tiendas;
+CREATE TABLE tiendas (
+        id_tienda INT AUTO_INCREMENT PRIMARY KEY,
+        id_usuario INT NOT NULL,
+        id_estado_tienda INT NOT NULL,
+        nombre_tienda VARCHAR(100) NOT NULL,
+        logo_tienda VARCHAR(256) NOT NULL,
+        descripcion VARCHAR(256) NOT NULL,
+        nombre_banco VARCHAR(100) NOT NULL,
+        tipo_cuenta_bancaria VARCHAR(100) NOT NULL,
+        numero_de_cuenta VARCHAR(100) NOT NULL,
+        cci VARCHAR(100) NOT NULL,
+        FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+        FOREIGN KEY (id_estado_tienda) REFERENCES estados_tienda(id_estado_tienda)
+);
+
+DROP TABLE IF EXISTS categorias_productos;
+CREATE TABLE categorias_productos (
+        id_categoria_producto INT AUTO_INCREMENT PRIMARY KEY,
+        nombre_categoria_producto VARCHAR(256) NOT NULL
+);
+
+DROP TABLE IF EXISTS productos;
+CREATE TABLE productos (
+        id_producto INT AUTO_INCREMENT PRIMARY KEY,
+        id_tienda INT NOT NULL,
+        id_categoria_producto INT NOT NULL,
+        nombre_producto VARCHAR(256) NOT NULL,
+        descripcion_producto TEXT NOT NULL,
+        ruta_foto_principal VARCHAR(256) NOT NULL,
+        precio FLOAT NOT NULL,
+        stock INT NOT NULL,
+        FOREIGN KEY (id_tienda) REFERENCES tiendas(id_tienda) ON DELETE CASCADE,
+        FOREIGN KEY (id_categoria_producto) REFERENCES categorias_productos(id_categoria_producto)
+);
+
+DROP TABLE IF EXISTS fotos_galeria_producto;
+CREATE TABLE fotos_galeria_producto (
+        id_foto_galeria INT AUTO_INCREMENT PRIMARY KEY,
+        id_producto INT NOT NULL,
+        ruta_foto VARCHAR(256) NOT NULL,
+        FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+);
+
+DROP TABLE IF EXISTS estados_pedidos;
+CREATE TABLE estados_pedidos (
+        id_estado_pedido INT AUTO_INCREMENT PRIMARY KEY,
+        nombre_estado_pedido VARCHAR(256) NOT NULL
+);
+
+DROP TABLE IF EXISTS pedidos;
+CREATE TABLE pedidos (
+        id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+        id_usuario INT NOT NULL,
+        id_tienda INT NOT NULL,
+        id_producto INT NOT NULL,
+        id_estado_pedido INT NOT NULL,
+        cantidad INT NOT NULL,
+        fecha_y_hora VARCHAR(256) NOT NULL,
+        total INT NOT NULL,
+        FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+        FOREIGN KEY (id_tienda) REFERENCES tiendas(id_tienda),
+        FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
+        FOREIGN KEY (id_estado_pedido) REFERENCES estados_pedidos(id_estado_pedido)
+);
+
+
+INSERT INTO estados_tienda (nombre_estado_tienda) VALUES('Activo'),('Inactivo'),('Pendiente de aprobación'),('Suspendido');
+INSERT INTO rol (nombre) VALUES('admin'),('user'),('seller');
+INSERT INTO estados_pedidos (nombre_estado_pedido) VALUES('Pendiente'),('En camino'),('Entregado'),('Cancelado');
